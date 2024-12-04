6bd0a0311c1b3a22e8c8a3869706ab73a3ef4c3d
Nayabsaleem <nayabsaleem280@gmail.com>
2024-12-04 21:56:35 +0500

    first commit

---
+++ puzzles.js
@@ -0,0 +1,17 @@
+let a = 45;
+let b = prompt("guess the number");
+if (a==b ){
+    document.write("Congrates you are win");
+}
+else if (a == --b){
+    document.write("You are too close");
+}
+else if (a == ++b){
+    dpcumnet.write("You are too close");
+}
+else if (a > b){
+    document.write("The number is too small")
+}
+else if (a<b){
+    document.write("The is number too large")
+}
